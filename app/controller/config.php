273edd8a64e1017fd80845d7b5355592cff83033
SophieKhuu <166718926+SophieKhuu@users.noreply.github.com>
2024-06-07 14:09:47 +0200

    ajout fichiers config et fichier sql
    + base créé sur mysql

---
+++ app/controller/config.php
@@ -0,0 +1,46 @@
+
+<!-- ----- debut config -->
+<?php
+
+// Utile pour le débugage car c'est un interrupteur pour les echos et print_r.
+if (!defined('DEBUG')) {
+    define('DEBUG', FALSE);
+}
+
+// ===============
+// Configuration de la base de données sur dev-isi
+$dsn = 'mysql:dbname=khuusoph;host=localhost;charset=utf8';
+$username = 'khuusoph';
+$password = 'l53yFldD';
+
+if (!defined('LOCAL')) {
+    define('LOCAL', FALSE);
+}
+
+if (LOCAL) {
+    // Configuration de la base de données sur localhost
+    $dsn = 'mysql:dbname=CAVE;host=localhost;charset=utf8';
+    $username = 'root';
+    $password = '';
+}
+ 
+// chemin absolu vers le répertoire du projet SUR DEV-ISI 
+$root = dirname(dirname(__DIR__)) . "/";
+
+
+if (DEBUG) {
+ echo ("<ul>");
+ echo (" <li>dsn = $dsn</li>");
+ echo (" <li>username = $username</li>");
+ echo (" <li>password = $password</li>");
+ echo ("<li>---</li>");
+ echo (" <li>root = $root</li>");
+
+ echo ("</ul>");
+}
+?>
+
+<!-- ----- fin config -->
+
+
+
